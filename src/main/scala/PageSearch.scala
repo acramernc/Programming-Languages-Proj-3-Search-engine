dd5d989d60a0e7049963c6fc6e1df0a48756b463
CRAMERAM17 <acramernc@gmail.com>
2021-04-13 20:39:30 -0400

    made a super basic implementation of count

--- src/main/scala/PageSearch.scala
+++ src/main/scala/PageSearch.scala
@@ -3,7 +3,16 @@ import scala.collection.parallel.CollectionConverters._
 
 object PageSearch {
     def count(pages: List[RankedWebPage], query: List[String]): List[Double] = {
-        // TODO: complete implementation
+        var searchResult = scala.collection.mutable.ListBuffer.empty[Double]
+        for (page <- pages) {
+            var total = 0
+            for (q <- query) {
+                val count = page.text.sliding(q.length).count(_ == q) //count the number of times q appears in page
+                total += count
+            }
+            searchResult.addOne(total)
+        }
+        searchResult.toList
     }
 
     def tf(pages: List[RankedWebPage], query: List[String]): List[Double] = {
