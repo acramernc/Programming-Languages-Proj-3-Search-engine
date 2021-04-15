914f4d1abb4c56e4d8dd47d3e066d6f741fb1090
CRAMERAM17 <acramernc@gmail.com>
2021-04-15 13:22:48 -0400

    made PageSearch parallel

--- src/main/scala/PageSearch.scala
+++ src/main/scala/PageSearch.scala
@@ -3,21 +3,21 @@ import scala.collection.parallel.CollectionConverters._
 
 object PageSearch {
     def count(pages: List[RankedWebPage], query: List[String]): List[Double] = {
-        pages.map((p: RankedWebPage) => {
-            (for (q <- query) yield p.text.split(q,-1).length - 1).sum
-        })
+        pages.par.map((p: RankedWebPage) => {
+            (for (q <- query) yield p.text.split(q,-1).length - 1).sum.toDouble
+        }).toList
     }
 
     def tf(pages: List[RankedWebPage], query: List[String]): List[Double] = {
-        pages.map((p: RankedWebPage) => {
+        pages.par.map((p: RankedWebPage) => {
             (for (q <- query) yield p.text.split(q,-1).length - 1).sum /  p.text.length.toDouble
-        })
+        }).toList
     }
 
     def tfidf(pages: List[RankedWebPage], query: List[String]): List[Double] = {
-        pages.map((p: RankedWebPage) => {
+        pages.par.map((p: RankedWebPage) => {
             (for (q<-query) yield tf(List[RankedWebPage](p), List[String](q))(0) * idf(pages, q)).sum
-        })
+        }).toList
     }
 
     def idf(pages: List[RankedWebPage], term:String): Double ={
